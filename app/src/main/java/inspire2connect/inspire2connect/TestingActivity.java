2d34ba81b30bfa6bef04b68aab8b5e03cba60bf8
Pradeep Kumar <pradeep20036@iiitd.ac.in>
2021-06-17 20:14:51 +0530

    user profile updated

---
+++ app/src/main/java/inspire2connect/inspire2connect/TestingActivity.java
@@ -0,0 +1,14 @@
+package inspire2connect.inspire2connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_user_new);
+    }
+}
