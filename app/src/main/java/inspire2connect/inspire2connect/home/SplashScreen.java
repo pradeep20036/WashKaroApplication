a3cc312c01a6804c8c36343bdefb61fe552f8103
sudeep20097 <sudeep20097@iiitd.ac.in>
2021-07-21 13:45:46 +0530

    splash screen activity created

---
+++ app/src/main/java/inspire2connect/inspire2connect/home/SplashScreen.java
@@ -0,0 +1,16 @@
+package inspire2connect.inspire2connect.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import inspire2connect.inspire2connect.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
