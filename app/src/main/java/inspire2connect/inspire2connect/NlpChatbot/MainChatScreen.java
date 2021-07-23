82ba37123889823078cc5b2d8451d7f2d7daf58f
Pradeep Kumar <pradeep20036@iiitd.ac.in>
2021-07-23 11:30:51 +0530

    Language Translation has been integrated....

--- app/src/main/java/inspire2connect/inspire2connect/NlpChatbot/MainChatScreen.java
+++ app/src/main/java/inspire2connect/inspire2connect/NlpChatbot/MainChatScreen.java
@@ -7,11 +7,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +22,7 @@ import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -28,11 +31,17 @@ import java.util.Objects;
 import inspire2connect.inspire2connect.R;
 import inspire2connect.inspire2connect.home.HomeActivity;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainChatScreen extends AppCompatActivity {
     FloatingActionButton sendbutton;
@@ -44,7 +53,7 @@ public class MainChatScreen extends AppCompatActivity {
     TextView tv_back_button;
     private static final int REQUEST_CODE_SPEECH_INPUT = 1;
     ShapeableImageView iv_mic;
-
+    Switch tg_hindi;
 
     private ArrayList<MessageClass> messageClassArrayList;
     @Override
@@ -57,8 +66,13 @@ public class MainChatScreen extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         tv_back_button= findViewById(R.id.tv_back);
 
+        tg_hindi=findViewById(R.id.tg_switch);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         USER = user.getUid();
+
+
+
         // adding speech to text converter
         iv_mic=findViewById(R.id.mic);
 
@@ -116,6 +130,21 @@ public class MainChatScreen extends AppCompatActivity {
 
                 if(msg !="")
                 {
+
+
+                    if(tg_hindi.isChecked()){
+                        // if message is in hindi , then convert it in english... and then give it to the send
+                        // message function
+                        String reply[]=new String[1];
+                        try {
+                            getTranslatedString(reply,msg,"hindi","english");
+                            msg=reply[0];
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
                     sendMessage(msg);
 
 //                    Toast.makeText(MainChatScreen.this,"Message sent",Toast.LENGTH_SHORT).show();
@@ -131,6 +160,10 @@ public class MainChatScreen extends AppCompatActivity {
 
 
     }
+
+
+
+
     // result for mic activity
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
@@ -153,7 +186,47 @@ public class MainChatScreen extends AppCompatActivity {
         }
     }
 
+// language translation model By Vineet Integration & code writing
+    public void getTranslatedString(String [] reply,String input,String source, String target) throws IOException {
 
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            //your codes here
+
+
+            String server = "http://1.1.1.216:7584";
+            String payload = "/translate?source="+source+"&target="+target+"&text="+input;
+            String url = server + payload;
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            OkHttpClient client = new OkHttpClient();
+
+            okhttp3.Call call = client.newCall(request);
+            okhttp3.Response response = call.execute();
+
+
+            if (response.isSuccessful()) {
+                String myResponse = response.body().string();
+//                    System.out.println(myResponse);
+                try {
+                    JSONObject obj = new JSONObject(myResponse);
+                    String ans = obj.get("output") + "";
+                    String escapedStr = ans;
+                    String hindiStr = StringEscapeUtils.unescapeJava(escapedStr);
+
+                    reply[0] = hindiStr;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 
 
 
@@ -173,7 +246,7 @@ public class MainChatScreen extends AppCompatActivity {
 //        http://fd8926d6aa3f.ngrok.io
         OkHttpClient okHttpClient = new OkHttpClient();
         Retrofit retrofit =new Retrofit.Builder()
-                .baseUrl("http://ac0717fae23b.ap.ngrok.io/webhooks/rest/")
+                .baseUrl("http://c03107cd3d1d.ap.ngrok.io/webhooks/rest/")
                 .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -196,8 +269,24 @@ public class MainChatScreen extends AppCompatActivity {
                     if (response.body().size() > 1) {
                         for (int i = 1; i <= response.body().size(); i++) {
                             BotResponse botResponse = response.body().get(i - 1);
-                            Log.d("CHECK", "" + botResponse.text);
-                            messageClassArrayList.add(new MessageClass(botResponse.text, BOT));
+                            // bot will always reply in english... if hindi is toggled then it should display in hindi
+
+                            String bot_response=botResponse.text;
+
+                            if(tg_hindi.isChecked()){
+                                // if message is in hindi , then convert it in english... and then give it to the send
+                                // message function
+                                String reply[]=new String[1];
+                                try {
+                                    getTranslatedString(reply,bot_response,"english","hindi");
+                                    bot_response=reply[0];
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            Log.d("CHECK", "" + bot_response);
+                            messageClassArrayList.add(new MessageClass(bot_response, BOT));
                             adapter.notifyDataSetChanged();
                             recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
 
@@ -205,7 +294,21 @@ public class MainChatScreen extends AppCompatActivity {
                     } else {
                         BotResponse botResponse = response.body().get(0);
                         Log.d("CHECK", "" + botResponse.text);
-                        messageClassArrayList.add(new MessageClass(botResponse.text, BOT));
+                        String bot_response=botResponse.text;
+
+                        if(tg_hindi.isChecked()){
+                            // if message is in hindi , then convert it in english... and then give it to the send
+                            // message function
+                            String reply[]=new String[1];
+                            try {
+                                getTranslatedString(reply,bot_response,"english","hindi");
+                                bot_response=reply[0];
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        messageClassArrayList.add(new MessageClass(bot_response, BOT));
                         adapter.notifyDataSetChanged();
                         recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
 
